566548c746ee18c3c15e4745ec5a1d749ea9c759
Mihkel Tiganik <mtiganik@gmail.com>
2023-03-02 01:53:09 +0200

    ex 5 working

--- ex5.py
+++ ex5.py
@@ -1,16 +1,10 @@
-f = open("input.txt")
-
-data =""
-for x in f:
-    data = data + x
-
-def sort_file(inp, insep = ",", inend = "\n", outsep="in", outend="in"):
+def sort_file(inp,*, insep = ",", inend = "\n", outsep=",", outend="\n"):
     arr = inp.split("\n")
-    
-    #[x for x in arr if x ]
+
     while '' in arr:
         arr.remove('')
     arr = [x for x in arr if not x.startswith("#")]
+
     change = True
     while change == True:
         change = False
@@ -20,6 +14,7 @@ def sort_file(inp, insep = ",", inend = "\n", outsep="in", outend="in"):
                 arr[i] = arr[i+1]
                 arr[i+1] = temp
                 change = True
+
     listToStr = '\n'.join([str(elem) for elem in arr])
     if outsep != "in":
         listToStr= listToStr.replace(",",outsep)
@@ -27,12 +22,26 @@ def sort_file(inp, insep = ",", inend = "\n", outsep="in", outend="in"):
         listToStr= listToStr.replace("\n", outend)
     return listToStr
 
-print(sort_file(data))
-print("######")
-print(sort_file('b\ny\na'))
-print("######")
-print(sort_file('b,b\ny,y\na,a', outsep=':', outend='\t'))
-print("######")
+try:
+    sort_file('abc', "test") # -> TypeError
+    raise Exception("2 positional arguments not allowed")
+
+except TypeError: pass
+
+# Example result
+assert sort_file("2,3,d\n1,4,d\n8,2,z\n2,4,x\n2,4,a") == "1,4,d\n2,3,d\n2,4,a\n2,4,x\n8,2,z"
+
+# A string with only one item per row
+assert sort_file('b\ny\na') == 'a\nb\ny'
+
+# Providing some keyword arguments
+assert sort_file('b,b\ny,y\na,a', outsep=':', outend='\t') == 'a:a\tb:b\ty:y'
+
+# Blank lines and lines starting with '#' should not appear in the result
+assert sort_file('b,q\n\n#p,y\na,o', outsep='-') == 'a-o\nb-q'
+
+# Proper sorting and ignore blank lines at the end
+assert sort_file('2,3,d\n2,4,x\n2,4,a\n\n\n', outend=' -- end\n') == '2,3,d -- end\n2,4,a -- end\n2,4,x'
+
+print("End of program")
 print(sort_file('b,q\n\n#p,y\na,o', outsep='-'))
-print("######")
-print(sort_file('2,3,d\n2,4,x\n2,4,a\n\n\n', outend=' -- end\n'))
